649451f03e6ed99d85e95b2acb12ac3cc1db41f9
Egil <egil.moller@freecode.no>
2018-12-12 17:41:56 +0100

    Code cleanup

--- SisterAlarm.ino
+++ SisterAlarm.ino
@@ -3,45 +3,52 @@
 #include <LiquidCrystal.h>
 #include <MicroLCD.h>
 
-LCD_SH1106 lcd;
+/* You will have to change this:
 
+ * Princess Ophelias card id. To figure out the id of your card so you
+ * can update this, program the Arduino and swipe your card. The card
+ * id will be displayed on the screen.
+ */
+byte ownerCardId[4] = {0x3a, 0xf4, 0x7f, 0xb9};
+
+/* You might have to change this if you're using a different Arduino
+ * model:
+
+ * Our Arduino pin mapping
+ */
 #define SS_PIN 10
-#define RST_PIN 9
- 
-MFRC522 rfid(SS_PIN, RST_PIN); // Instance of the class
+#define RST_PIN 9 // Not really used, but required by the rfid module
+#define BUZZER_PIN 6
+#define PIR_PIN 9
 
+LCD_SH1106 lcd; 
+MFRC522 rfid(SS_PIN, RST_PIN);
 MFRC522::MIFARE_Key key; 
 
-// Init array that will store new NUID 
-byte nuidPICC[4] = {0x3a, 0xf4, 0x7f, 0xb9};
 
-#define buzzerPin  6
-
-bool is_engaged;
-
-#define pirPin 9
-bool pirlastval = LOW;
+// Current state
+bool isEngaged = 0; // Is the alarm engaged and ready to beep if the PIR sensor is activated?
+bool pirLastVal = LOW; // Previous state of the PIR sensor, used to detect changes in its state.
 
 void setup() { 
-  pinMode(buzzerPin, OUTPUT);
-  digitalWrite(buzzerPin, LOW);
+  pinMode(BUZZER_PIN, OUTPUT);
+  digitalWrite(BUZZER_PIN, LOW);
 
   lcd.begin();
+  SPI.begin();
+  rfid.PCD_Init();
+  // We print everything to the serial too for easier debugging...
   Serial.begin(9600);
-  SPI.begin(); // Init SPI bus
-  rfid.PCD_Init(); // Init MFRC522 
 
   for (byte i = 0; i < 6; i++) {
     key.keyByte[i] = 0xFF;
   }
 
-  is_engaged = 0;
-
-  display_state();
+  displayState();
 }
 
-void display_state() {
-  if (is_engaged) {
+void displayState() {
+  if (isEngaged) {
     Serial.println(F("engaged"));
     lcd.clear();
     lcd.setFontSize(FONT_SIZE_MEDIUM);
@@ -61,13 +68,13 @@ void display_state() {
 }
  
 void loop() {
-  bool pirinval;
+  bool pirInVal;
   
-  if (is_engaged) {
-    pirinval = digitalRead(pirPin);
-    if (pirinval != pirlastval) {
-      if (pirinval) {
-        digitalWrite(buzzerPin, HIGH);
+  if (isEngaged) {
+    pirInVal = digitalRead(PIR_PIN);
+    if (pirInVal != pirLastVal) {
+      if (pirInVal) {
+        digitalWrite(BUZZER_PIN, HIGH);
 
         lcd.clear();
         lcd.setCursor(0, 0);
@@ -79,30 +86,28 @@ void loop() {
       
         Serial.println("UNDER ATTACK");
       } else {
-        digitalWrite(buzzerPin, LOW);
-        display_state();
+        digitalWrite(BUZZER_PIN, LOW);
+        displayState();
         Serial.println("END OF ATTACK");
       }
-      pirlastval = pirinval;
+      pirLastVal = pirInVal;
     }
   }
 
-  // Look for new cards
-  if ( ! rfid.PICC_IsNewCardPresent())
+  if (!rfid.PICC_IsNewCardPresent())
+    return;
+  if (!rfid.PICC_ReadCardSerial())
     return;
 
-  // Verify if the NUID has been readed
-  if ( ! rfid.PICC_ReadCardSerial())
-    return;
-
-//  Serial.print(F("PICC type: "));
   MFRC522::PICC_Type piccType = rfid.PICC_GetType(rfid.uid.sak);
-//  Serial.println(rfid.PICC_GetTypeName(piccType));
 
-  // Check is the PICC of Classic MIFARE type
   if (piccType != MFRC522::PICC_TYPE_MIFARE_MINI &&  
     piccType != MFRC522::PICC_TYPE_MIFARE_1K &&
     piccType != MFRC522::PICC_TYPE_MIFARE_4K) {
+
+    // The card is not a Classic MIFARE card, so we just tell the
+    // sneaky snooper to try something smarter than their old bus
+    // card, right?
 
     lcd.clear();
     lcd.setCursor(0, 0);
@@ -113,10 +118,10 @@ void loop() {
     return;
   }
 
-  if (rfid.uid.uidByte[0] != nuidPICC[0] || 
-    rfid.uid.uidByte[1] != nuidPICC[1] || 
-    rfid.uid.uidByte[2] != nuidPICC[2] || 
-    rfid.uid.uidByte[3] != nuidPICC[3] ) {
+  if (rfid.uid.uidByte[0] != ownerCardId[0] || 
+    rfid.uid.uidByte[1] != ownerCardId[1] || 
+    rfid.uid.uidByte[2] != ownerCardId[2] || 
+    rfid.uid.uidByte[3] != ownerCardId[3] ) {
 
     Serial.println(F("You sneaky snooper!\nYour tag:"));
     printHex(rfid.uid.uidByte, rfid.uid.size);
@@ -131,7 +136,7 @@ void loop() {
     lcdPrintHex(rfid.uid.uidByte, rfid.uid.size);
   }
   else {
-    if (is_engaged) {
+    if (isEngaged) {
       lcd.clear();
       lcd.setCursor(0, 0);
       lcd.setFontSize(FONT_SIZE_SMALL);
@@ -149,17 +154,19 @@ void loop() {
       Serial.println(F("Clear the area"));
     }
     delay(2000);
-    is_engaged = !is_engaged;
+    isEngaged = !isEngaged;
 
-    display_state();
+    displayState();
   }
 
-  // Halt PICC
+  // Close/stop the RFID reader and clean up its state
   rfid.PICC_HaltA();
-
-  // Stop encryption on PCD
   rfid.PCD_StopCrypto1();
 }
+
+/* Helper routines to print a card id as hex to the lcd and the serial
+ * port.
+ */
 
 void lcdPrintHex(byte *buffer, byte bufferSize) {
   for (byte i = 0; i < bufferSize; i++) {
@@ -168,23 +175,9 @@ void lcdPrintHex(byte *buffer, byte bufferSize) {
   }
 }
 
-
-/**
- * Helper routine to dump a byte array as hex values to Serial. 
- */
 void printHex(byte *buffer, byte bufferSize) {
   for (byte i = 0; i < bufferSize; i++) {
     Serial.print(buffer[i] < 0x10 ? " 0" : " ");
     Serial.print(buffer[i], HEX);
-  }
-}
-
-/**
- * Helper routine to dump a byte array as dec values to Serial.
- */
-void printDec(byte *buffer, byte bufferSize) {
-  for (byte i = 0; i < bufferSize; i++) {
-    Serial.print(buffer[i] < 0x10 ? " 0" : " ");
-    Serial.print(buffer[i], DEC);
   }
 }
